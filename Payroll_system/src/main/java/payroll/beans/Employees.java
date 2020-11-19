1c15952f5c7d52a332eda9e507111f48fd3471e9
LuisCabral20 <35475671+LuisCabral20@users.noreply.github.com>
2020-11-19 16:16:56 -0600

    Update Employees.java
    Completed the Employees POJO

---
+++ Payroll_system/src/main/java/payroll/beans/Employees.java
@@ -0,0 +1,66 @@
+package payroll.beans;
+
+import java.time.LocalDate;
+
+import javax.persistence.Embeddable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class Employees {
+
+	private int idEmployees;
+	private String position;
+	private String salary;
+	private LocalDate dateHired;
+	private int empDepartment;
+	private String firstName;
+	private String lastName;
+
+	/**
+	 * A constructor with all but the date hired field.
+	 * 
+	 * @param id
+	 * @param position
+	 * @param salary
+	 * @param empDepartment
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Employees(int id, String position, String salary, int empDepartment, String firstName,
+			String lastName) {
+		this.idEmployees = id;
+		this.position = position;
+		this.salary = salary;
+		this.empDepartment = empDepartment;
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+
+	/**
+	 * Constructor that takes in all the arguments.
+	 * 
+	 * @param id
+	 * @param position
+	 * @param salary
+	 * @param dateHired
+	 * @param empDepartment
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Employees(int id, String position, String salary, LocalDate dateHired, int empDepartment,
+			String firstName, String lastName) {
+		this.idEmployees = id;
+		this.position = position;
+		this.salary = salary;
+		this.dateHired = dateHired;
+		this.empDepartment = empDepartment;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
+
